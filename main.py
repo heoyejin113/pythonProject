78b0894663dc365066b13167676f29b9e75e970a
heoyejin113 <hyj5829@gmail.com>
2022-05-31 21:41:11 +0900

    init

--- main.py
+++ main.py
@@ -3,12 +3,11 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import pandas as pd
+csv_file = pd.DataFrame(pd.read_csv("C:\\Users\\hyj58\\Downloads\\top20.csv", sep=",", header=0, index_col=False))
+csv_file.to_json("C:\\Users\\hyj58\\Downloads\\file.json", orient="records", date_format="epoch", double_precision=10,
+                     force_ascii=True, date_unit="ms", default_handler=None)
+df=pd.read_json("C:\\Users\\hyj58\\Downloads\\file.json")
+data = pd.DataFrame(df)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(data[['licenseOrgan']])
