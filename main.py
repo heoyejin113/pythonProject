9f79b0b292564032c5217dcbbd28b1df09a62234
heoyejin113 <hyj5829@gmail.com>
2022-05-31 21:52:52 +0900

    한글설정

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 import pandas as pd
 csv_file = pd.DataFrame(pd.read_csv("C:\\Users\\hyj58\\Downloads\\top20.csv", sep=",", header=0, index_col=False))
 csv_file.to_json("C:\\Users\\hyj58\\Downloads\\file.json", orient="records", date_format="epoch", double_precision=10,
-                     force_ascii=True, date_unit="ms", default_handler=None)
+                     force_ascii=False, date_unit="ms", default_handler=None)
 df=pd.read_json("C:\\Users\\hyj58\\Downloads\\file.json")
 data = pd.DataFrame(df)
 
